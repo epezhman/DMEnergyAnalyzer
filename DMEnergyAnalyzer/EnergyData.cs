5d57806c3316562d9c004a91df6644a0194d9b04
Pezhman Nasirifard <epezhman@gmail.com>
2016-07-18 18:19:29 +0200

    Add project files.

---
+++ DMEnergyAnalyzer/EnergyData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DMEnergyAnalyzer
+{
+    public class EnergyData
+    {
+        public string MyProperty { get; set; }
+    }
+}
---
+++ DMEnergyAnalyzer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Management;
+using System.Windows.Forms;
+
+namespace WMISample
+{
+    public class MyWMIQuery
+    {
+        public static void Main()
+        {
+            try
+            {
+                ManagementObjectSearcher searcher =
+                     new ManagementObjectSearcher("root\\WMI",
+                     "SELECT * FROM MSWmi_ProviderInfo");
+
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    Console.WriteLine("-----------------------------------");
+                    Console.WriteLine("MSWmi_ProviderInfo instance");
+                    Console.WriteLine("-----------------------------------");
+                    Console.WriteLine("Active: {0}", queryObj["Active"]);
+                    Console.WriteLine("Description: {0}", queryObj["Description"]);
+                    Console.WriteLine("FriendlyName: {0}", queryObj["FriendlyName"]);
+                    Console.WriteLine("InstanceName: {0}", queryObj["InstanceName"]);
+                    Console.WriteLine("Location: {0}", queryObj["Location"]);
+                    Console.WriteLine("Manufacturer: {0}", queryObj["Manufacturer"]);
+                    Console.WriteLine("Service: {0}", queryObj["Service"]);
+                }
+
+
+
+
+
+
+
+
+                Console.ReadLine();
+            }
+            catch (ManagementException e)
+            {
+                MessageBox.Show("An error occurred while querying for WMI data: " + e.Message);
+            }
+        }
+    }
+}
